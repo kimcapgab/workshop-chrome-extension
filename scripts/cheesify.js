8457a720050e6d636ec29864acce91cf4e6184b5
kimcapgab <90987981+kimcapgab@users.noreply.github.com>
2022-10-06 09:19:12 -0400

    Commiting first chrome extension

--- scripts/cheesify.js
+++ scripts/cheesify.js
@@ -1,6 +1,17 @@
 function cheesify() {
   // TODO: Add the image replacement script here
+  document.querySelectorAll('img').forEach(picture => {
+    picture.src = 
+        `https://source.unsplash.com/collection/8884129/${picture.width}x${picture.height}?${Math.random()}`;
+    picture.srcset = picture.src
+  })
 }
 
+
 // TODO: Write a function to listen for messages on the content page using chrome.runtime.onMessage
-  
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    console.log(request)
+   if (request.action === 'cheesify') cheesify();
+  }
+);
